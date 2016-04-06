a07789790b14b33fa534a274f6860c3e3f1f75e0
Tania Safavi <roksanamonsef@gmail.com>
2016-04-06 13:09:24 +0200

    This is my first changes in the print systems

--- src/main/java/CalculatorRoxana.java
+++ src/main/java/CalculatorRoxana.java
@@ -4,28 +4,28 @@ public class CalculatorRoxana {
 	
 		public double add(double a , double b){
 			double answer = a+b;
-			System.out.println(answer);
+			System.out.println("The answer of the adition is:  "+ answer);
 			return answer;
 		
 		}
 		
 		public double subtraction (double a , double b){
 			double answerSub = a-b;
-			System.out.println(answerSub);
+			System.out.println("The answer of the subtraction is:  "+ answerSub);
 			return answerSub;
 			
 		}
 		
 		public double multiplication (double a , double b){
 			double answerMult = a*b;
-			System.out.println(answerMult);
+			System.out.println("The answer of the multiplication is:  "+ answerMult);
 			return answerMult;
 			
 		} 
 		
 		public double division (double a , double b){
 			double answerDiv=a/b;
-			System.out.println(answerDiv);
+			System.out.println("The answer of the division is:  "+ answerDiv);
 			return answerDiv;
 			
 		}
