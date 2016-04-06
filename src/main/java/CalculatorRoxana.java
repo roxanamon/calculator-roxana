85527f0347aed62e9a6f8cb13f501d3c7f6061c8
Tania Safavi <roksanamonsef@gmail.com>
2016-04-06 12:01:06 +0200

    This is my first commit

---
+++ src/main/java/CalculatorRoxana.java
@@ -0,0 +1,35 @@
+
+public class CalculatorRoxana {
+
+	
+		public double add(double a , double b){
+			double answer = a+b;
+			System.out.println(answer);
+			return answer;
+		
+		}
+		
+		public double subtraction (double a , double b){
+			double answerSub = a-b;
+			System.out.println(answerSub);
+			return answerSub;
+			
+		}
+		
+		public double multiplication (double a , double b){
+			double answerMult = a*b;
+			System.out.println(answerMult);
+			return answerMult;
+			
+		} 
+		
+		public double division (double a , double b){
+			double answerDiv=a/b;
+			System.out.println(answerDiv);
+			return answerDiv;
+			
+		}
+		
+
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		CalculatorRoxana cal = new CalculatorRoxana();
+		
+		cal.add(2.5, 3.5);
+		cal.subtraction(5.5, 2.3);
+		cal.division(6.4, 2.2);
+		cal.multiplication(2.5, 2.5);
+		
+	}
+
+}
